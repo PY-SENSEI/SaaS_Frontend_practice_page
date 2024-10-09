9ab6a1a21b7c0719f782f89ddc4d072dcafd4f56
PY-SENSEI <gauravjena48@gmail.com>
2024-10-09 23:21:03 +0530

    design-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./sections/Header"
+import Hero from "./sections/Hero"
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   )
 }
